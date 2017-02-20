339377a92acaa18be0c5f8c3d67fea8bed2f552e
Yeghia Koronian <Yeghiakoronian@gmail.com>
2017-02-19 19:21:49 -0500

    Forgot to add the productlist component to @ng-module in app-module.ts

--- APM/app/app.component.js
+++ APM/app/app.component.js
@@ -8,7 +8,7 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 var core_1 = require("@angular/core");
 var AppComponent = (function () {
     function AppComponent() {
-        this.pageTitle = 'Yeghia Produt Management';
+        this.pageTitle = 'Yeghia Product Management';
     }
     return AppComponent;
 }());
--- APM/app/app.component.ts
+++ APM/app/app.component.ts
@@ -10,5 +10,5 @@ import { Component } from '@angular/core';
     `
 })
 export class AppComponent { 
-    pageTitle: string = 'Yeghia Produt Management';
+    pageTitle: string = 'Yeghia Product Management';
 }
