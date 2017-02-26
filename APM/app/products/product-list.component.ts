7de6f9526f2c07a73dfa4ad31d1fac6edfed5b9d
Yeghia Koronian <yeghia.koronian@nexonia.com>
2017-02-25 20:30:52 -0500

    created IProduct interface, to remove any[]

--- APM/app/products/product-list.component.ts
+++ APM/app/products/product-list.component.ts
@@ -1,8 +1,7 @@
 import { Component} from '@angular/core';
 import {Logger} from 'angular2-logger/core';
-
 import {LoggerService} from '../logging/logger-service';
-
+import {IProduct} from './product';
 @Component ({
     selector: 'pm-products',
     templateUrl: 'app/products/product-list.component.html'
@@ -14,7 +13,7 @@ export class ProductListComponent {
     imageMargin: number = 2;
     listFilter : string = 'cart';
     showImage: boolean = false;
-    products: any[] = [
+    products: IProduct[] = [
             {
                 "productId": 1,
                 "productName": "Leaf Rake",
