119b01db1f9e24f69ec7ffd718b4ef72858aa518
Yeghia Koronian <yeghiakoronian@gmail.com>
2017-03-05 22:31:48 -0500

    Using parameters in a route

--- APM/app/products/product-detail.component.ts
+++ APM/app/products/product-detail.component.ts
@@ -1,10 +1,22 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {IProduct} from './product';
+import {ActivatedRoute} from '@angular/router';
+
 
 @Component({
-    templateUrl: 'app/products/Product-detail.component.html';
+    templateUrl: 'app/products/Product-detail.component.html'
 })
-export class ProductDetailComponent {
+export class ProductDetailComponent implements OnInit{
     pageTitle: string = 'Product Detail';
     product: IProduct;
+
+    constructor(private _route: ActivatedRoute){
+    }
+
+    ngOnInit(): void{
+       let id = +this._route.snapshot.params['id'];
+       this.pageTitle += `:  ${id}`; 
+    }
+
+
 }
